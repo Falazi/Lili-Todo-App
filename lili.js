423150bf29b0ee16ab1b3605f64b4f69f0485007
Falazi <khkearns@sky.com>
2022-08-07 15:39:48 +0100

    Update lili.js
    It is different

--- lili.js
+++ lili.js
@@ -1,3 +1,10 @@
+
+// if smartmode is true, then the select function will be "smart"
+// this can be changed to false in the user script
+// or you can use selectStrict if you only want to do one time
+
+let smartmode = true
+
 function create(ele, text, id, putel, cless) {
 
     let el = document.createElement(ele);
@@ -8,17 +15,52 @@ function create(ele, text, id, putel, cless) {
         document.body.appendChild(el);
     }
 
-    if (id) { el.setAttribute("id", id) }
+    if (id) {
+        // removes the hashtag if you use it in the id parameter
+        if (id.charAt(0) === "#") {
+            id = id.substring(1)
+        }
+
+        el.setAttribute("id", id)
+    }
+
 
     if (cless) {
+        // u can use dot for class
+
+        if (cless.charAt(0) === ".") {
+            cless = cless.substring(1)
+        }
+
         el.setAttribute("class", cless)
     }
 
 }
 
-const select = document.querySelector.bind(document);
+const selectStrict = document.querySelector.bind(document);
+
+
+// this function is "smart"
+// if an element is not found, it will try to get an id then a class if it fails
+
+function select(el) {
+    let ele = document.querySelector(el)
+
+    if (!ele && smartmode === true) {
+        return ele = document.querySelector(`#${el}`)
+    }
+
+    else if (!ele && smartmode === true) {
+        return ele = document.querySelector(`.${el}`)
+    }
+
+    return ele
+}
+
+
 
 const selectId = document.getElementById.bind(document)
+
 
 // selects every element that matches
 const selectAll = document.querySelectorAll.bind(document)
@@ -36,13 +78,16 @@ ele.setAttribute(id, value)
 // the format is like this
 // set("#cool", { "type": "number", "class": "my-class" });
 
-function set(el, options) {
-    let ele = select(el)
+function setAttr(el, options) {
+    setAttrutil(select(el), options)
+}
+
+function setAttrutil(ele, options) {
     Object.keys(options).forEach(function (attr) {
         ele.setAttribute(attr, options[attr]);
     })
-}
 
+}
 
 
 // usage of onEvent Function
@@ -75,7 +120,114 @@ function onEvent(el, eventt, func) {
 }
 
 function remove(el) {
-    const ele = select(el)
-    ele.remove()
+    select(el).remove()
+}
+
+
+function toggleClass(el, cless) {
+    select(el).classList.toggle(cless);
+}
+
+
+// moves an element to inside another element (the bottom of it)
+function move(el, to) {
+    select(to).appendChild(select(el))
+}
+
+// just in case you forget that move does this
+function moveBottom(el, to) {
+    move(el, to)
+}
+
+// moves an element above a container
+function moveAbove(el, to) {
+
+    // Get the reference element
+    let ele = select(el)
+    // Get the parent element
+    let parentDiv = ele.parentNode
+
+    // Insert the new element into before sp2
+    parentDiv.insertBefore(ele, select(to))
+
+}
+
+
+
+
+// moves an element below another element (outside of it)
+function moveBelow(el, to) {
+    // grab target element reference
+    const target = select(to);
+
+    // insert the element after target element
+    target.parentNode.insertBefore(select(el), target.nextSibling);
+
+}
+
+
+// moves an element to the top of another element
+function moveTop(el, to) {
+    var container = select(to);
+    container.insertBefore(select(el), container.firstChild);
+}
+
+
+function addClass(el, cless) {
+    select(el).classList.add(cless);
+}
+
+function removeClass(el, cless) {
+    select(el).classList.remove(cless)
+}
+
+
+// gets the text of an element
+function getText(el) {
+    return select(el).innerText
+}
+
+
+function setText(el, text) {
+    select(el).innerText = text
+}
+
+function appendText(el, text) {
+    let ele = select(el)
+    ele.innerText = ele.innerText + text
+}
+
+
+function prependText(el, text) {
+    let ele = select(el)
+    ele.innerText = text + ele.innerText
+}
+
+function hide(el) {
+    select(el).style.display = "none"
+}
+
+function show(el) {
+    select(el).style.display = ""
+}
+
+
+
+// you can set the original value of an element, and it will be set as an attribute
+function setOrigvalue(el, val) {
+    select(el).setAttribute("origValue", val)
+}
+
+
+// you can access the orig value
+function getOrigvalue(el) {
+    return select(el).getAttribute("origValue");
+}
+
+
+
+// why does javascript use dots for everything? too much work
+function log(str) {
+    console.log(str)
 }
 
